f87807d033ad93a162c2e00470f8ab10d41afc09
Tsar Belyash <fev1792@gmail.com>
2024-07-02 13:25:35 +0300

    Pulling a database from the backend

--- CRM_BVI/CRM_BVI/settings.py
+++ CRM_BVI/CRM_BVI/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CRM_BVI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / database/ 'update_sql.py',
     }
 }
 
