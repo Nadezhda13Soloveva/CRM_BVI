84dc0a32592fe11b1ba906430ee9b1d83dd49117
Nadezhda <nadezhda13soloveva@mail.ru>
2024-07-03 12:43:38 +0300

    UPD: Add format and validate checking, fix true output. Need to fix how to find link to diploma

--- parsing/hash_version.py
+++ parsing/hash_version.py
@@ -1,8 +1,9 @@
-import os
 import re
-import pandas as pd
-import requests
 import hashlib
+import requests
+import os
+import pandas as pd
+
 
 # Путь к Excel-файлу
 abiturients_file = 'abiturients.xlsx'
@@ -10,7 +11,7 @@ bvi_file = 'bvi_mai.xlsx'
 
 # Загрузка данных из Excel-файла
 data = pd.read_excel(abiturients_file)
-data.iloc[:, :6] = data.iloc[:, :6].fillna('-')
+data.iloc[:, :6] = data.iloc[:, :6].fillna('')
 data.iloc[:, 6:] = data.iloc[:, 6:].fillna(0)
 bvi = pd.read_excel(bvi_file)
 
@@ -18,16 +19,22 @@ bvi = pd.read_excel(bvi_file)
 abiturients = []
 olimpiads = []
 
-
 # Функция проверки вариантов написания имён с е/ё
 def generate_word_variants(word):
-    # Генерирует варианты слова с заменой 'Е' на 'Ё' не более одного раза.
+    # Генерирует варианты слова с заменой 'Е' на 'Ё' не более одного раза."""
     variants = [word]
     indices = [i for i, letter in enumerate(word) if letter == 'Е']
 
     for idx in indices:
         variant_list = list(word)
         variant_list[idx] = 'Ё'
+        variants.append(''.join(variant_list))
+
+    indices = [i for i, letter in enumerate(word) if letter == 'е']
+
+    for idx in indices:
+        variant_list = list(word)
+        variant_list[idx] = 'ё'
         variants.append(''.join(variant_list))
 
     return variants
@@ -52,13 +59,6 @@ def generate_name_variants(name):
     return combined_variants
 
 
-# Функция для генерации хэша
-def generate_hash(last_name, first_name, middle_name, birth_date):
-    namestring = f"{last_name} {first_name} {middle_name} {birth_date}"
-    return hashlib.sha256(namestring.encode()).hexdigest()
-
-
-# Функция для выполнения поиска и сбора данных
 def search_diplomas(year, hashed):
     url = f'https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-20{year}/by-person-released/{hashed}/codes.js'
     response = requests.get(url)
@@ -74,30 +74,86 @@ def search_diplomas(year, hashed):
         return {}
 
 
+def format_name(name):
+    if " " in name:
+        name = name.split(" ")
+        name = " ".join([i.capitalize() for i in name])
+    elif "-" in name:
+        name = name.split("-")
+        name = "-".join([i.capitalize() for i in name])
+    else:
+        name = name.capitalize()
+    return name
+
+
+def validate_name_and_date(last_name, first_name, middle_name, birth_date):
+    # Регулярное выражение для имени (Фамилия Имя Отчество), включая "-" и пробелы (не более 1 пробела)
+    name_pattern = re.compile(r"^[А-Яа-яЁёA-Za-z-]+( [А-Яа-яЁёA-Za-z-]+)?$")
+
+    # Регулярное выражение для даты (ГГГГ-ММ-ДД)
+    date_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+
+    # Проверка фамилии, имени и отчества по регулярному выражению
+    last_name_match = name_pattern.match(last_name)
+    first_name_match = name_pattern.match(first_name)
+    middle_name_match = True if middle_name == "" else bool(name_pattern.match(middle_name))
+
+    # Проверка даты по регулярному выражению
+    date_match = date_pattern.match(birth_date)
+
+    # Убедимся, что все части имени и дата корректны
+    return (bool(last_name_match) and
+            bool(first_name_match) and
+            bool(middle_name_match) and
+            bool(date_match))
+
+
 id_abiturient = id_olimpiada = 1
 # Проход по всем записям в Excel-файле
 for index, row in data.iterrows():
-    full_name = row['Фамилия']
-    first_name = row['Имя']
-    middle_name = row['Отчество']
+    last_name = format_name(row['Фамилия'])
+    first_name = format_name(row['Имя'])
+    middle_name = format_name(row['Отчество'])
     birth = row['Дата рождения'].strftime('%d.%m.%Y').split('.')
-    birthday = birth[0]
-    birthmonth = birth[1]
-    birthyear = birth[2]
+    bdd = birth[0]
+    bdm = birth[1]
+    bdy = birth[2]
 
-    if any(row[col] >= 75 for col in ['ЕГЭ русский язык', 'ЕГЭ математика', 'ЕГЭ физика', 'ЕГЭ информатика']):
+    if len(bdd) == 1:
+        bdd = f'0{bdd}'
+    if len(bdm) == 1:
+        bdm = f'0{bdm}'
+
+    birth = '-'.join([bdy, bdm, bdd])
+    is_valid = validate_name_and_date(last_name, first_name, middle_name, birth)
+
+    if is_valid and any(row[col] >= 75 for col in ['ЕГЭ русский язык', 'ЕГЭ математика', 'ЕГЭ физика', 'ЕГЭ информатика']):
         is_olimp = False
-        print(f"Ищем дипломы для: {full_name} {first_name} {middle_name}, Дата рождения: {'.'.join(birth)}")
+        print(f"Ищем дипломы для: {last_name} {first_name} {middle_name}, Дата рождения: {birth}")
 
-        # сюда проверка на е-ё
-        name_variants = generate_name_variants(' '.join([full_name, first_name, middle_name]))
+        #сюда проверка на е-ё
+        name_variants = generate_name_variants(' '. join([last_name, first_name, middle_name]))
 
         for name in name_variants:
             print(name)
-            full_name, first_name, middle_name = name.split(" ")
-            hashed = generate_hash(full_name, first_name, middle_name, f'{birthday}.{birthmonth}.{birthyear}')
+            to_hash = ' '.join([name, birth])
+
+            while "  " in to_hash:
+                to_hash = to_hash.replace("  ", " ")
+
+            hashed = hashlib.sha256(to_hash.encode()).hexdigest()
+
             for year in range(20, 25):
-                page_content = search_diplomas(year, hashed)
-                if page_content:
-                    print(page_content)
+                diplomas = search_diplomas(year, hashed)
+                if diplomas:
+                    print(year, diplomas)
+
+
+
+
+
+
+
+
+
 
