443c9a2fbcccdad1294cfd780985ea8ee0f0061d
Tsar Belyash <fev1792@gmail.com>
2024-07-04 15:32:08 +0300

    First attempt to connect a page with a call. ERRORS: it is required to correct display errors in html file, and also to configure displaying of list of blocks instead of one.

--- CRM_BVI/checking/urls.py
+++ CRM_BVI/checking/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('calling/', views.Call.as_view(), name='calling'),
+    path('calling/', views.CallList.as_view(), name='calling'),
 ]
 
--- CRM_BVI/checking/views.py
+++ CRM_BVI/checking/views.py
@@ -5,5 +5,7 @@ from django.views import generic
 def index(request):
     return render(request, 'index.html')
     
-class Call(generic.ListView):
+class CallList(generic.ListView):
     model = Abiturients
+    context_object_name = 'calling_list'
+    template_name = 'calling.html'
