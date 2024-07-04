77dda87376d04e605bb6cab1ff761298f8ba181c
Lilia-Chechina <146482951+Lilia-Chechina@users.noreply.github.com>
2024-07-04 12:06:45 +0300

    Create admin.py
    Обновлённый admin.py

---
+++ admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Abiturients, Olimpiads, Directions
+
+admin.site.register(Abiturients)
+admin.site.register(Olimpiads)
+admin.site.register(Directions)
