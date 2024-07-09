10b8f60ac1717053fb454afb01bf524c2baa3968
Tsar Belyash <fev1792@gmail.com>
2024-07-09 16:45:35 +0300

    Integration of Lily's code. Database update is now working. Added a new table with directions.

--- CRM_BVI/checking/models.py
+++ CRM_BVI/checking/models.py
@@ -32,8 +32,6 @@ class Abiturients(models.Model):
     def __str__(self):
         """Строка для представления объекта MyModelName (например, в административной панели и т.д.)."""
         return f"{self.last_name} {self.first_name} {self.middle_name},\nномер: {self.phone_number},\n{self.email}\nЕГЭ: {self.ege_russian}|{self.ege_math}|{self.ege_physics}|{self.ege_informatics}|{self.status}\n\n{self.call_result}" + "{}".format(self.birth_date.strftime('%d.%m.%Y'))
-    def get_absolute_url(self):
-        return reverse('abi-detail-view', args=[str(self.id)])
 
 
 class Olimpiads(models.Model):
@@ -52,24 +50,22 @@ class Olimpiads(models.Model):
     # Methods
     def __str__(self):
         return f"{self.id}\n{self.name}|{self.year}\n{self.diploma_file}"
-    def get_absolute_url(self):
-        return reverse('olimp-detail-view', args=[str(self.id)])
 
 class Directions(models.Model):
     # Поля
     id = models.IntegerField(primary_key=True)
     abiturient = models.ForeignKey(Abiturients, on_delete=models.CASCADE)
-    snils = models.CharField(max_length=20)
+    snils = models.CharField(max_length=30)
     sum_balls = models.IntegerField()
     sum_balls_ege = models.IntegerField()
-    doc = models.CharField(max_length=20)  # Копия или Оригинал
-    egpu_orig = models.CharField(max_length=20)  # ✓ или пусто
-    dormitory = models.CharField(max_length=20)  # ✓ или пусто
-    direction_1 = models.CharField(max_length=20)
-    direction_2 = models.CharField(max_length=20)
-    direction_3 = models.CharField(max_length=20)
-    direction_4 = models.CharField(max_length=20)
-    direction_5 = models.CharField(max_length=20)
+    doc = models.CharField(max_length=30)
+    egpu_orig = models.CharField(max_length=30)
+    dormitory = models.CharField(max_length=30)
+    direction_1 = models.CharField(max_length=30)
+    direction_2 = models.CharField(max_length=30)
+    direction_3 = models.CharField(max_length=30)
+    direction_4 = models.CharField(max_length=30)
+    direction_5 = models.CharField(max_length=30)
 
     # Метаданные
     class Meta:
@@ -77,10 +73,6 @@ class Directions(models.Model):
 
     # Methods
     def __str__(self):
-        # тут можно вывести больше информации, но пока так
-        return f"{self.id}\nSNILS: {self.snils},\nСумма баллов с ИД: {self.sum_balls}\nНаправления: {self.direction_1}|{self.direction_2}|{self.direction_3}|{self.direction_4}|{self.direction_5}\n\nОбщежитие{self.dormitory}"
-    
-    def get_absolute_url(self):
-        return reverse('direction-detail-view', args=[str(self.id)])
+        return f"{self.id}\n{self.direction_1}|{self.direction_2}\n{self.direction_3}"
         
 
--- CRM_BVI/database/update_sql.py
+++ CRM_BVI/database/update_sql.py
@@ -1,79 +1,82 @@
-import sqlite3
 import pandas as pd
+import sqlite3
 
-abiturients_file = 'results_abiturients.xlsx'
-olimpiads_file = 'results_olimpiads.xlsx'
+# # Функция для обновления таблицы в базе данных
+# def update_table_from_excel(conn, df, table_name):
+#     cursor = conn.cursor()
+#     cursor.execute(f"DELETE FROM {table_name}")
+#     df.to_sql(table_name, conn, if_exists='append', index=False)
+#     conn.commit()
 
-# Загрузка данных из Excel-файла
-abiturients = pd.read_excel(abiturients_file)
-olimpiads = pd.read_excel(olimpiads_file)
+# Чтение данных из Excel-файлов
+file_abiturients = 'results_abiturients.xlsx'
+file_olimpiads = 'results_olimpiads.xlsx'
+file_directions = 'directions.xlsx'
+
+df_abiturients = pd.read_excel(file_abiturients)
+df_olimpiads = pd.read_excel(file_olimpiads)
+df_directions = pd.read_excel(file_directions)
 
 # Подключение к базе данных SQLite
 conn = sqlite3.connect('abiturients_olimp.db')
-c = conn.cursor()
+cursor = conn.cursor()
 
-# Создание таблицы, если она еще не существует
-c.execute('''
-CREATE TABLE IF NOT EXISTS Abiturients (
-    abiturient_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    last_name VARCHAR(50) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    middle_name VARCHAR(50),
-    birth_date DATE NOT NULL,
-    phone_number VARCHAR(15) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    ege_russian INTEGER,
-    ege_math INTEGER,
-    ege_physics INTEGER,
-    ege_informatics INTEGER,
-    status VARCHAR(50) CHECK (status IN ('Сомневается', 'Точно поступает', 'Точно не поступает', 'Нет информации')),
-    call_result TEXT
-)
+# Создание таблиц
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS checking_abiturients (
+        id INTEGER PRIMARY KEY,
+        last_name TEXT,
+        first_name TEXT,
+        middle_name TEXT,
+        birth_date DATE,
+        phone_number TEXT,
+        email TEXT,
+        ege_russian INTEGER,
+        ege_math INTEGER,
+        ege_physics INTEGER,
+        ege_informatics INTEGER,
+        status TEXT,
+        call_result TEXT
+    )
 ''')
 
-c.execute('''
-CREATE TABLE IF NOT EXISTS Olimpiads (
-    olympiads_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    abiturient_id INTEGER NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    year INTEGER,
-    diploma_file VARCHAR(255) NOT NULL,
-    FOREIGN KEY (abiturient_id) REFERENCES Abiturients (id) ON DELETE CASCADE
-)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS checking_olimpiads (
+        id INTEGER PRIMARY KEY,
+        abiturient_id INTEGER,
+        name TEXT,
+        year INTEGER,
+        diploma_file TEXT,
+        FOREIGN KEY(abiturient_id) REFERENCES checking_abiturients(id)
+    )
 ''')
 
-# Список словарей для проверки и обновления
-abiturients = abiturients.to_dict('records')
-olimpiads = olimpiads.to_dict('records')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS checking_directions (
+        id INTEGER PRIMARY KEY,
+        abiturient_id INTEGER,
+        snils TEXT,
+        sum_balls INTEGER,
+        sum_balls_ege INTEGER,
+        doc TEXT,
+        egpu_orig TEXT,
+        dormitory TEXT,
+        direction_1 TEXT,
+        direction_2 TEXT,
+        direction_3 TEXT,
+        direction_4 TEXT,
+        direction_5 TEXT
+    )
+''')
+
+# Загрузка данных из pandas DataFrame в таблицы базы данных SQLite
+df_abiturients.to_sql('checking_abiturients', conn, if_exists='replace', index=False)
+df_olimpiads.to_sql('checking_olimpiads', conn, if_exists='replace', index=False)
+df_directions.to_sql('checking_directions', conn, if_exists='replace', index=False)
 
 
-# Функция для обновления или добавления информации о абитуриенте
+conn.commit()  # это включает изменения, такие как создание таблиц и вставка данных
+# позволяет группировать несколько операций в одну транзакцию
 
-def update_or_insert_bd(bd, entrant):
-    # Проверка наличия информации о человеке в базе данных
-    c.execute(f'SELECT * FROM {bd} WHERE id = ?', (entrant['id'],))
-    result = c.fetchone()
-    if result:
-        # Если информация о человеке содержится, но значения ключей отличаются
-        if result != tuple(entrant.values()):
-            columns = ', '.join(f"{k} = ?" for k in entrant.keys())
-            values = list(entrant.values()) + [entrant['id']]
-            c.execute(f'UPDATE {bd} SET {columns} WHERE id = ?', values)
-    else:
-        # Если информация о человеке не содержится, добавление новой записи
-        placeholders = ', '.join('?' * len(entrant))
-        columns = ', '.join(entrant.keys())
-        c.execute(f'INSERT INTO {bd} ({columns}) VALUES ({placeholders})', list(entrant.values()))
-
-
-# Проход по списку словарей и обновление базы данных
-for bd in (abiturients, olimpiads):
-    for entrant in bd:
-        if bd == abiturients:
-            update_or_insert_bd('Abiturients', entrant)
-        else:
-            update_or_insert_bd('Olimpiads', entrant)
-
-# Сохранение изменений и закрытие соединения
-conn.commit()
+# Закрытие соединения
 conn.close()
