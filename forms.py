3d6f3deb83fd5a2c6e89c5759ba7a0e5eeb20fbd
Lilia-Chechina <146482951+Lilia-Chechina@users.noreply.github.com>
2024-07-09 20:19:53 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
+# forms.py
 from django import forms
 from .models import Abiturients # Импорт модели из списка Abiturients
 
 class Abiturients_FilterForm(forms.Form):
-    statuses_ = forms.MultipleChoiceField(
+    options = forms.MultipleChoiceField(
         choices=Abiturients.STATUSES, #  выборы из модели Abiturients
         widget=forms.CheckboxSelectMultiple,  # Отображаем как чекбоксы
         required=False # сама не знаю зачем
