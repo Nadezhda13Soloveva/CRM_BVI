79e45bca738ae91f28b18c75e95b9eff4cf74a06
Lilia-Chechina <146482951+Lilia-Chechina@users.noreply.github.com>
2024-07-10 08:13:35 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,29 +1,54 @@
-# views.py
 from django.shortcuts import render
-from .models import Abiturients
-from .forms import Abiturients_FilterForm  # Импортируем форму
+from .models import Abiturients, Directions
+from .forms import AbiturientsFilterForm
 
-def abiturients_filter_list(request):
-    form = Abiturients_FilterForm(request.GET or None)  # Инициализируем форму, передавая в нее GET параметры запроса
-    # Получаем всех абитуриентов из базы данных
-    abiturients = Abiturients.objects.all()  # Получение всех абитуриентов
+inst_direc = {
+    1: ['24.03.04', '24.05.07', '25.03.01'],
+    2: ['13.03.01', '24.05.02'],
+    3: ['09.03.01', '09.03.02', '09.03.03', '09.03.04', '12.03.04', '13.03.02', '24.05.06', '27.03.04', '27.03.05'],
+    4: ['10.03.01', '10.05.02', '11.05.01'],
+    5: ['38.03.01', '38.03.02', '45.03.02'],
+    6: ['05.03.06', '12.03.04', '24.05.01', '27.03.03', '24.05.05'],
+    7: ['24.05.05', '27.03.03'],
+    8: ['01.03.02', '01.03.04', '02.03.02'],
+    10: ['45.03.02', '42.03.01'],
+    11: ['12.03.04', '22.03.01', '22.03.02'],
+}
 
-    if form.is_valid():  # Проверяем, является ли форма валидной
-        # Получаем выбранные пользователем параметры из формы
-        options_statuses_ = form.cleaned_data.get('options')
-        # Если пользователь выбрал какие-либо параметры, фильтруем абитуриентов по этим параметрам
-        if options_statuses_:
-            abiturients = abiturients.filter(status__in=options_statuses_)  # фильтр по полю statuses
 
-    # Создание контекста, который будет передан в шаблон
-    context = {
-        'form': form,  # Форма фильтра
-        'abiturients': abiturients # Отфильтрованный список абитуриентов
-    }
-    # используем функцию render, чтобы отобразить шаблон product_list.html, передав в него контекст с формой и продуктами
-    return render(request, 'C:\Projects\CRM_BVI-frontend\CRM_BVI-frontend\CRM_BVI\checking\abiturients_list.html', context)
+def filter_abiturients(request):
+    if request.method == 'GET':
+        form = AbiturientsFilterForm(request.GET)  # Создаем экземпляр формы и передаем данные из запроса GET.
+        # Если данных нет, то передаем None.
+        abiturients = Abiturients.objects.all()  # получение всех объектов Abiturients
 
-# selected_statuses = ['сомневается']
-# products = Abiturients.objects.all()  # Получаем все продукты
-# print(products.filter(status__in=selected_statuses))  # Фильтруем продукты
+        if form.is_valid():  # проверка валидности формы:
+            status = form.cleaned_data.get('status')
+            direction_keys = form.cleaned_data.get('direction_key')
+            #  содержат выбранные значения статусов и ключей направлений соотв
 
+            if status:  # если пользователь выбрал статусы, фильтруем абитуриентов по этим статусам
+                abiturients = abiturients.filter(status__in=status)
+
+            if direction_keys:  # если пользователь выбрал ключи направлений
+                direction_codes = [] # Создаем список direction_codes, который содержит
+                # все направления для выбранных ключей.
+                for key in direction_keys:
+                    direction_codes.extend(inst_direc[int(key)])
+                # Фильтруем абитуриентов по каждому из полей направлений (direction_1, direction_2, и т.д.),
+                # используя объединение (|) запросов для получения абитуриентов, которые имеют хотя бы одно из
+                # выбранных направлений.
+                abiturients = abiturients.filter(directions__direction_1__in=direction_codes) | \
+                              abiturients.filter(directions__direction_2__in=direction_codes) | \
+                              abiturients.filter(directions__direction_3__in=direction_codes) | \
+                              abiturients.filter(directions__direction_4__in=direction_codes) | \
+                              abiturients.filter(directions__direction_5__in=direction_codes)
+                # abiturients = abiturients.filter(
+                #     models.Q(directions__direction_1__in=direction_codes) |
+                #     models.Q(directions__direction_2__in=direction_codes) |
+                #     models.Q(directions__direction_3__in=direction_codes) |
+                #     models.Q(directions__direction_4__in=direction_codes) |
+                #     models.Q(directions__direction_5__in=direction_codes)
+                # ).distinct()
+                # Метод distinct() используется для устранения дубликатов в результате объединения запросов
+    return render(request, 'abiturients_list.html', {'form': form, 'abiturients': abiturients})
